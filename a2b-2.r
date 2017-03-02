38dcebebe3a069884baff32263a95e214f3329b8
Duncan Pulsifer <duncanpulsifer@gmail.com>
2017-03-01 20:05:26 -0400

    Adding R unigram and bigram solutions for Q2

---
+++ a2b-2.r
@@ -0,0 +1,59 @@
+library(datasets)
+library(tm)
+library(SnowballC)
+library(RWeka)
+library(NLP)
+library(e1071)
+library(Matrix)
+library(class)
+
+options(mc.cores=1)
+
+setwd("/Users/duncanpulsifer/Documents/Dalhousie/CSCI\ 3151/Assignments/a2")
+
+corpus <- Corpus(DirSource("NewsGroups", encoding = "UTF-8", recursive = TRUE),		readerControl = list(language = "en"))
+
+#preprocess corpus
+corpus <- tm_map (corpus, content_transformer(tolower))
+corpus <- tm_map (corpus, removeNumbers)
+corpus <- tm_map (corpus, removePunctuation)
+corpus <- tm_map (corpus, removeWords, stopwords("english"))
+corpus <- tm_map (corpus, stemDocument)
+
+#convert to tf-idf weighted document term matrix
+dtmUnigram <- DocumentTermMatrix(corpus, control = list(weighting = function(x) weightTfIdf(x, normalize = TRUE)))
+
+#convert to matrix and then dataframe
+unigramModel <- as.matrix(dtmUnigram)
+unigramModel <- as.data.frame(unigramModel)
+
+#append training column and assign 1 or 0 in 70/30 proportion
+unigramModel[,"training"] <- ifelse(runif(nrow(unigramModel))<0.70,1,0)
+
+#add category labels to rows
+unigramModel$Category <- NA
+for ( i in 1:nrow(unigramModel) ) {
+	if ( 1 <= i & i <= 40 ) { unigramModel$Category[i] <- 1 }
+	if ( 40 < i & i <= 80 ) { unigramModel$Category[i] <- 2 }
+	if ( 80 < i & i <= 120 ) { unigramModel$Category[i] <- 3 }
+	if ( 120 < i & i <= 160 ) { unigramModel$Category[i] <- 4 }
+}
+
+#get number of training indication column for set division
+trainingColumnNumber <- grep("training", names(unigramModel))
+
+#divide training and testing sets
+trainingUnigramModel <- unigramModel[unigramModel$training==1,-trainingColumnNumber]
+testingUnigramModel <- unigramModel[unigramModel$training==0,-trainingColumnNumber]
+
+#run naive bayes algorithm
+unigramNB <- naiveBayes(as.factor(Category)~., data = trainingUnigramModel)
+
+#get number of training indication column
+categoryColumnNumber <- grep("Category", names(testingUnigramModel))
+
+#run predictions with testing data and generate confusion matrix
+unigramNB_Test <- predict(unigramNB, testingUnigramModel[,-categoryColumnNumber], na.action = na.pass)
+confusion_matrix <- table(pred=unigramNB_Test, true=testingUnigramModel$Category)
+
+print(confusion_matrix)
---
+++ a2b-2bigram.r
@@ -0,0 +1,66 @@
+library(datasets)
+library(tm)
+library(SnowballC)
+library(RWeka)
+library(NLP)
+library(e1071)
+library(Matrix)
+library(class)
+
+#options(mc.cores=1)
+
+setwd("/Users/duncanpulsifer/Documents/Dalhousie/CSCI\ 3151/Assignments/a2")
+
+corpus <- Corpus(DirSource("NewsGroups", encoding = "UTF-8", recursive = TRUE),		readerControl = list(language = "en"))
+
+#preprocess corpus
+corpus <- tm_map (corpus, content_transformer(tolower))
+corpus <- tm_map (corpus, removeNumbers)
+corpus <- tm_map (corpus, removePunctuation)
+corpus <- tm_map (corpus, removeWords, stopwords("english"))
+corpus <- tm_map (corpus, stemDocument)
+
+#bigram tokenizer
+bigramTokenizer <- BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
+
+#convert to tf-idf weighted document term matrix
+dtmBigram <- DocumentTermMatrix(corpus, control = list(tokenize = bigramTokenizer, weighting = function(x) weightTfIdf(x, normalize = TRUE)))
+
+#convert to matrix and then dataframe
+bigramModel <- as.matrix(dtmBigram)
+bigramModel <- as.data.frame(bigramModel)
+
+#append training column and assign 1 or 0 in 70/30 proportion
+bigramModel[,"training"] <- ifelse(runif(nrow(bigramModel))<0.70,1,0)
+
+#add category labels to rows
+bigramModel$Category <- NA
+for ( i in 1:nrow(bigramModel) ) {
+	if ( 1 <= i & i <= 40 ) { bigramModel$Category[i] <- 1 }
+	if ( 40 < i & i <= 80 ) { bigramModel$Category[i] <- 2 }
+	if ( 80 < i & i <= 120 ) { bigramModel$Category[i] <- 3 }
+	if ( 120 < i & i <= 160 ) { bigramModel$Category[i] <- 4 }
+}
+
+#get number of training indication column for set division
+trainingColumnNumber <- grep("training", names(bigramModel))
+
+#divide training and testing sets
+trainingBigramModel <- bigramModel[bigramModel$training==1,-trainingColumnNumber]
+testingBigramModel <- bigramModel[bigramModel$training==0,-trainingColumnNumber]
+
+print(dtmBigram)
+
+#run naive bayes algorithm
+#bigramNB <- naiveBayes(as.factor(Category)~., data = trainingBigramModel)
+
+
+
+#get number of training indication column
+#categoryColumnNumber <- grep("Category", names(testingBigramModel))
+
+#run predictions with testing data and generate confusion matrix
+#bigramNB_Test <- predict(bigramNB, testingBigramModel[,-categoryColumnNumber], na.action = na.pass)
+#confusion_matrix <- table(pred=bigramNB_Test, true=testingBigramModel$Category)
+
+#print(confusion_matrix)
